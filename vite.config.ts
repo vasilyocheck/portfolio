c1ec9baa86702c5dd495b71863169d56d4b95379
vasilyocheck <vasily.medvedev1986@gmail.com>
2024-04-02 05:12:44 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    outDir: "build",
-  },
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
